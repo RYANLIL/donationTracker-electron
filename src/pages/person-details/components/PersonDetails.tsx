780994ccc0dc6e112ff38c8a44b262758025d656
RyanL <RYANLIL@github.com>
2024-04-18 21:02:56 -0400

    add grid item to personDetails component

--- src/pages/person-details/components/PersonDetails.tsx
+++ src/pages/person-details/components/PersonDetails.tsx
@@ -15,47 +15,57 @@ export default function PersonDetails(props: IPersonDetails) {
   return (
     <div style={{ border: "solid 2px" }}>
       <span>PERSON DETAILS</span>
-      <Grid gap={3}>
-        <TextField
-          id="firstName"
-          label="First Name"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            handleOnChange(e)
-          }
-          value={props.personDetails.firstName}
-        />
-        <TextField
-          id="lastName"
-          label="Last Name"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            handleOnChange(e)
-          }
-          value={props.personDetails.lastName}
-        />
-        <TextField
-          id="phone1"
-          label="Phone 1"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            handleOnChange(e)
-          }
-          value={props.personDetails.phone1}
-        />
-        <TextField
-          id="phone2"
-          label="Phone 2"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            handleOnChange(e)
-          }
-          value={props.personDetails.phone2}
-        />
-        <TextField
-          id="email"
-          label="Email"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            handleOnChange(e)
-          }
-          value={props.personDetails.email}
-        />
+      <Grid container gap={2}>
+        <Grid item>
+          <TextField
+            id="firstName"
+            label="First Name"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              handleOnChange(e)
+            }
+            value={props.personDetails.firstName}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="lastName"
+            label="Last Name"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              handleOnChange(e)
+            }
+            value={props.personDetails.lastName}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="phone1"
+            label="Phone 1"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              handleOnChange(e)
+            }
+            value={props.personDetails.phone1}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="phone2"
+            label="Phone 2"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              handleOnChange(e)
+            }
+            value={props.personDetails.phone2}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="email"
+            label="Email"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              handleOnChange(e)
+            }
+            value={props.personDetails.email}
+          />
+        </Grid>
       </Grid>
     </div>
   );
