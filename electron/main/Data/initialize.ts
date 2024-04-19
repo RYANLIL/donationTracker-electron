007cd416cb49968c1d193fda19031e97efdf4797
RyanL <RYANLIL@github.com>
2024-04-18 21:41:14 -0400

    Create db from sql file programmatically

--- electron/main/Data/initialize.ts
+++ electron/main/Data/initialize.ts
@@ -37,77 +37,31 @@ export default class InitDatabase {
       "SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%'"
     );
     let res = tables.all();
-
     if (res.length === 0) {
       try {
         //TODO: wrap create statements in transaction
-        this._db.exec(`CREATE TABLE "person" (
-            "id"		INTEGER PRIMARY KEY,
-            "firstName"	TEXT DEFAULT '' NOT NULL,
-            "lastName"	TEXT DEFAULT '' NOT NULL,
-            "phone1"	TEXT DEFAULT '' NOT NULL,
-            "phone2"	TEXT DEFAULT '' NOT NULL,
-	          "email"		TEXT DEFAULT '' NOT NULL,
-        
-            "isDeleted"	INTEGER DEFAULT 0 NOT NULL,
-            "createdAt" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            "deletedAt" TEXT DEFAULT '' NOT NULL	
+        let createTablesSQL = readFileSync(
+          join(ADD_RESOURCES, "/create/createALL.sql"),
+          "utf8"
         );
-        
-        CREATE TABLE "address" (
-            "id"			INTEGER PRIMARY KEY,
-            "fk_personId" 	INTEGER NOT NULL,
-            "address1"		TEXT DEFAULT '' NOT NULL,
-            "address2"		TEXT DEFAULT '' NOT NULL,
-            "address3"		TEXT DEFAULT '' NOT NULL,
-            "city"			TEXT DEFAULT '' NOT NULL,
-            "province"		TEXT DEFAULT '' NOT NULL,
-            "country"		TEXT DEFAULT '' NOT NULL,
-            "postalCode"	TEXT DEFAULT '' NOT NULL,
-        
-            "isDeleted"		INTEGER DEFAULT 0 NOT NULL,
-            "createdAt" 	TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            "deletedAt" 	TEXT DEFAULT '' NOT NULL,
-            FOREIGN KEY("fk_personId") REFERENCES person("id")
-        );
-        
-        CREATE TABLE "donation_records" (
-            "id"			INTEGER PRIMARY KEY,
-            "fk_personId"	INTEGER NOT NULL, --foreign key
-            "amount"		REAL DEFAULT 0 NOT NULL,
-            "date"			TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
-        
-            "isDeleted"		INTEGER DEFAULT 0 NOT NULL,
-            "createdAt" 	TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            "deletedAt" 	TEXT DEFAULT '' NOT NULL,
-            FOREIGN KEY("fk_personId") REFERENCES person("id")
-        );
-        
-        
-        CREATE TABLE "receipt_records" (
-            "id"			INTEGER PRIMARY KEY,
-            "fk_personId" 	INTEGER NOT NULL,
-            "amount" 		REAL DEFAULT 0 NOT NULL ,
-            "datePrinted"	TEXT DEFAULT '' NOT NULL,
-            "isPrinted" 	INTEGER DEFAULT 0 NOT NULL,
-        
-            "isDeleted"		INTEGER DEFAULT 0 NOT NULL,
-            "createdAt" 	TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            "deletedAt" 	TEXT DEFAULT '' NOT NULL,
-            FOREIGN KEY("fk_personId") REFERENCES person("id")
-            );`);
+        this._db.exec(createTablesSQL);
         res = tables.all();
       } catch (error) {
         if (!this._db.inTransaction) throw error; // (transaction was forcefully rolled back)
       }
     }
-
     console.log(res);
   }
 
   insertMockData() {
     let mockSQL = readFileSync(
       join(ADD_RESOURCES, "/mock-data/Person_MOCK_DATA.sql"),
+      "utf8"
+    );
+    this._db.exec(mockSQL);
+    mockSQL = "";
+    mockSQL = readFileSync(
+      join(ADD_RESOURCES, "/mock-data/Address_MOCK_DATA.sql"),
       "utf8"
     );
     this._db.exec(mockSQL);
