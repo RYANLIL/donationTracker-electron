63729da727f618e911713de2295a7dc52149c7c4
RyanL <RYANLIL@github.com>
2024-06-09 17:32:22 -0400

    (fix) Update version. * Set backup retention to 10 from 20

--- additional-resources/create/user_settings.sql
+++ additional-resources/create/user_settings.sql
@@ -2,6 +2,6 @@ CREATE TABLE "user_settings" (
 	"id"			    INTEGER PRIMARY KEY,
 	"backupLocation"    TEXT DEFAULT '' NOT NULL,
     "lastBackUp"          TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    "numOfBackUpsToKeep"INTEGER DEFAULT 20 NOT NULL,
+    "numOfBackUpsToKeep"INTEGER DEFAULT 10 NOT NULL,
 	"lastUpdated" 	    TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
